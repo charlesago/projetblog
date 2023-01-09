0f25a8eb3f1774c3523b8df34094f9db36291fe6
charlesagostinelli <chacha.agostinelli@gmail.com>
2023-01-09 08:38:34 +0100

    update front-end

--- template/base.html.php
+++ template/base.html.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+
     <title>Blog</title>
 </head>
 <header>
@@ -30,11 +31,11 @@
         </div>
     </nav>
 </header>
-<body>
-<div class="container mt-5">
+<body class="bg-dark">
+<div class="container mt-5 ">
 
     <?= $pageContent ?>
 
 </div>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
--- template/posts/post.html.php
+++ template/posts/post.html.php
@@ -1,32 +1,39 @@
-<div class="post mt-3">
+<style>
+    @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css");
+</style>
+
+<div class="post mt-3 text-light ">
     <h3><?= $post["title"]?></h3>
     <p><?= $post['content'] ?></p>
+    <div class="d-flex justify-content-end ">
     <form method="post" action="delete-post.php">
-        <button class="btn-danger btn"><input class="btn btn-danger" name="id" type="hidden" value="<?= $post["id"]?>">DEL</button>
+        <button class="btn btn-outline-light me-2"><input class="btn btn-danger" name="id" type="hidden" value="<?= $post["id"]?>"><i class="bi bi-trash-fill"></i></button>
     </form>
-    <a href="update-post.php?id=<?= $post['id'] ?>" class="btn btn-primary ">Edit</a>
+    <a href="update-post.php?id=<?= $post['id'] ?>" class="btn btn-outline-light me-2 "><i class="bi bi-pencil"></i></a>
 
-    <a href="index.php" class="btn btn-success">retour</a>
+    <a href="index.php" class="btn btn-outline-light"><i class="bi bi-skip-backward"></i></a>
 </div>
-
+</div>
 <div class="mt-5 fs-5 text-primary">
     <p>Comments</p>
 </div>
-<div class="col-12 mt-3 row d-flex p-2 bg-dark bg-opacity-10 border border-dark border-start-2 rounded-end">
+<div class="col-12 mt-3 row d-flex p-2 bg-dark bg-opacity-10 border border-light border-start-2 rounded-end text-light">
     <?php foreach ($comments as $comment): ?>
+<div >
+        <hr class="mt-3">
+
+        <p><strong class=""><?= $comment['content'] ?></strong></p>
+        <a href="#" class="btn btn-outline-light me-2"><strong><i class="bi bi-trash-fill"></i></strong></a>
+        <a href="#" class="btn btn-outline-light me-2 "><strong><i class="bi bi-pencil"></i></strong></a>
 
         <hr>
 
-        <p><strong><?= $comment['content'] ?></strong></p>
-        <a href="#" class="btn btn-danger"><strong>X</strong></a>
-        <a href="#" class="btn btn-warning"><strong>update</strong></a>
-
-        <hr>
-        <hr>
+</div>
 
     <?php endforeach; ?>
 
     <hr>
+</div>
 <div>
     <form method="post" class="form" action="createcomments.php">
         <input class="form-control"  type="text" name="content" placeholder="add a comment">
